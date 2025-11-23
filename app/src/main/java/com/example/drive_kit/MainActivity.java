1ad2f118a96f38f6453b51f963938a02fa68b514
Itamar000Git <itamarbabai98@gmail.com>
2025-11-23 18:18:23 +0200

    commit1

---
+++ app/src/main/java/com/example/drive_kit/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.drive_kit;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import com.example.drive_kit.SignUp;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button signinButton;
+    private Button signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //usernameEditText = findViewById(R.id.usernameEditText);
+        //passwordEditText = findViewById(R.id.passwordEditText);
+        //signinButton = findViewById(R.id.loginButton);
+        signupButton = findViewById(R.id.signupButton);
+
+        signupButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignUp.class);
+            startActivity(intent);
+        });
+    }
+}
